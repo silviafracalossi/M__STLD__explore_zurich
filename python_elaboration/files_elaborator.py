0b6652ea3a78fbdf1273d5e127f181f3482a4085
Fracalossi Silvia <silvia.fracalossi@stud-inf.unibz.it>
2020-06-27 18:14:50 +0200

    Not found a way to split bus line field in sql - used python

--- python_elaboration/files_elaborator.py
+++ python_elaboration/files_elaborator.py
@@ -7,7 +7,69 @@ from shapely.geometry import Point, Polygon
 from shape_creator import string_to_point, convert_point
 
 # Elaborates csv and stores elaboration with district id
-def elaborate_csv(districts, original_file, id_point_field, id_secondary_point_field=0):
+def elaborate_csv(districts, original_file, id_point_field):
+    print("[INFO] Processing csv file "+original_file)
+    new_file_lines = []
+
+    # Reading the district csv
+    with open('resources/data/01_original/'+original_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        header = ""
+
+        # Iterating the csv
+        for row in csv_reader:
+
+            # Header
+            if header == "":
+                header = row
+                header[0] = header[0].replace("\"","")
+                header.append("fixed_geometry")
+
+            # Rows
+            else:
+
+                # Convert location into point
+                location_point = ""
+                location_point = string_to_point(row[id_point_field])
+
+
+                # Iterate through districts to find the correct match
+                district_id = 0
+                for i in range(1, len(districts)):
+                    if location_point.within(districts[i]):
+                        district_id = i
+                        break
+
+                # Insert district id inside the row
+                if district_id != 0:
+                    row.append(location_point)
+                    row.append(district_id)
+                    new_file_lines.append([row])
+
+    # Destination file - name preparation
+    destination_file = original_file.replace("-", "elaborated")
+    destination_file = destination_file.replace(" ", "_")
+
+    # Writing into the destination file
+    with open('resources/data/02_python_elaborated/'+destination_file, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+        # Inserting header
+        header.append("district_id")
+        spamwriter.writerow(header)
+
+        # Inserting rows
+        for line in new_file_lines:
+            spamwriter.writerow(line[0])
+
+# Elaborates csv and stores elaboration with district id
+def elaborate_bus_csv(districts):
+
+    # Setting method variables
+    original_file = "03_04 - bus.csv"
+    id_point_field = 14
+    id_secondary_point_field = 15
+
     print("[INFO] Processing csv file "+original_file)
     new_file_lines = []
 
@@ -52,18 +114,32 @@ def elaborate_csv(districts, original_file, id_point_field, id_secondary_point_f
     # Destination file - name preparation
     destination_file = original_file.replace("-", "elaborated")
     destination_file = destination_file.replace(" ", "_")
+    header.append("district_id")
 
     # Writing into the destination file
     with open('resources/data/02_python_elaborated/'+destination_file, 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
 
         # Inserting header
-        header.append("district_id")
         spamwriter.writerow(header)
 
         # Inserting rows
         for line in new_file_lines:
             spamwriter.writerow(line[0])
+
+    # Creating the busline file
+    bus_line_file = "04_elaborated_only_busline.csv"
+    with open('resources/data/02_python_elaborated/'+bus_line_file, 'w', newline='') as csvfile:
+
+        spamwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+
+        # Inserting header
+        spamwriter.writerow(header)
+
+        # Inserting rows
+        for line in new_file_lines:
+            for value in line[0][13].split(','):
+                spamwriter.writerow([line[0][0], value])
 
 # Elaborates json and stores elaboration with district id
 def elaborate_json(districts, original_file):
@@ -103,7 +179,6 @@ def elaborate_json(districts, original_file):
     with open('resources/data/02_python_elaborated/'+destination_file, 'w') as json_dest_file:
         json.dump({"poi": new_file_lines}, json_dest_file)
 
-
 # Elaborates Train Station json differently
 def elaborate_train_station_json(districts):
     original_file = "05 - train station.json"
@@ -142,7 +217,6 @@ def elaborate_train_station_json(districts):
     # Writing rows in new file
     with open('resources/data/02_python_elaborated/'+destination_file, 'w') as json_dest_file:
         json.dump({"stations": new_file_lines}, json_dest_file)
-
 
 # Copying the neighbourhood file in the destination folder
 def copy_neighbourhoods():
--- python_elaboration/main.py
+++ python_elaboration/main.py
@@ -3,11 +3,11 @@ import shapely
 
 from shapely.geometry import Point, Polygon
 from districts_loader import load_districts
-from files_elaborator import elaborate_csv, elaborate_json, elaborate_train_station_json, copy_neighbourhoods
+from files_elaborator import elaborate_csv, elaborate_json, elaborate_train_station_json, copy_neighbourhoods, elaborate_bus_csv
 
 districts = load_districts()                                # 01 - district.csv
 copy_neighbourhoods()                                       # 02 - neighbourhood.csv
-elaborate_csv(districts, "03_04 - bus.csv", 14, 15)
+elaborate_bus_csv(districts)
 elaborate_train_station_json(districts)                     # 05 - train station.csv
 elaborate_csv(districts, "06 - car parking.csv", 60)
 elaborate_csv(districts, "07 - bike parking.csv", 12)
